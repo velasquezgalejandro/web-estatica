0842a203f0fed841ae456276a7a0acc13f7c4774
velasquezgalejandro <alejandro.velasquez2004@gmail.com>
2024-10-26 01:18:25 -0500

    se crea funcion para el boton agregar de tematica

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const button = document.getElementById("boton-agregar");
+
 document.addEventListener("DOMContentLoaded", function () {
   const toggleButton = document.getElementById("toggle-button");
   const navMenu = document.getElementById("nav-menu");
@@ -5,4 +7,16 @@ document.addEventListener("DOMContentLoaded", function () {
   toggleButton.addEventListener("click", function () {
     navMenu.classList.toggle("active"); // Cambia la clase para mostrar/ocultar el menú
   });
+});
+
+button.addEventListener("click", () => {
+  if (button.textContent.includes("Agregar")) {
+    button.textContent = "Agregado ✔";
+    button.classList.remove("boton-agregar");
+    button.classList.add("boton-agregado");
+  } else {
+    button.textContent = "Agregar +";
+    button.classList.remove("boton-agregado");
+    button.classList.add("boton-agregar");
+  }
 });
